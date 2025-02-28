e45fb19b02688d02f10e1568cb42262a52449803
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-02-28 18:39:38 -0500

    fix: remove unused attribute
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- trie.go
+++ trie.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Blink Labs Software
+// Copyright 2025 Blink Labs Software
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -21,7 +21,6 @@ import (
 
 type Trie struct {
 	rootNode Node
-	size     int
 }
 
 func NewTrie() *Trie {
